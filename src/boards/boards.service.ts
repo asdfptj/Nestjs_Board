96dac7e35c840debca74150c9ddc42282f06256d
asdfptj <taejunb70@gmail.com>
2023-04-19 23:04:45 +0900

    id error fix

--- src/boards/boards.service.ts
+++ src/boards/boards.service.ts
@@ -4,6 +4,7 @@ import { v1 as uuid } from 'uuid';
 import { CreateBoardDto } from './dto/create-board.dto';
 import { BoardRepository } from './board.repository';
 import { InjectRepository } from '@nestjs/typeorm';
+import { Board } from './board.entity';
 
 @Injectable()
 export class BoardsService {
@@ -26,6 +27,14 @@ export class BoardsService {
   //   this.boards.push(board);
   //   return board;
   // }
+
+  async getBoardById(id: number): Promise<Board> {
+    const found = await this.boardRepository.findOneBy({ id });
+
+    if (!found) throw new NotFoundException(`Can't find Board with id ${id}`);
+    return found;
+  }
+
   // getBoardById(id: string): Board {
   //   const found = this.boards.find((board) => board.id === id);
   //   if (!found) {
