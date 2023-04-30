7126097c108508121ac7d9b00a28903e3182153b
asdfptj <taejunb70@gmail.com>
2023-04-30 23:56:14 +0900

    repository error

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -7,6 +7,6 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 @Module({
   imports: [TypeOrmModule.forFeature([UserRepository])],
   controllers: [AuthController],
-  providers: [AuthService],
+  providers: [AuthService, UserRepository],
 })
 export class AuthModule {}
